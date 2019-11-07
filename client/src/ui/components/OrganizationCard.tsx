29625f0e45b4e531f46dc5162519a1d9b77d9992
nerocui <nerocui@outlook.com>
2019-11-07 02:29:50 -0800

    covered color null case.

--- client/src/ui/components/OrganizationCard.tsx
+++ client/src/ui/components/OrganizationCard.tsx
@@ -3,15 +3,19 @@ import useStyle from '../../style/organizationCard';
 import { hexToRgb } from '../../utils/StringUtils';
 
 const OrganizationCard = (props: any) => {
-    const color: any = hexToRgb('#'+props.color);
-    const backgroundColor: string = 'rgb(' + color.r.toString() + ',' + color.g.toString() + ',' + color.b.toString + ')';
+    let { color } = props;
+    if (!color) {
+        color = '03b1fc';
+    }
+    const colorObj: any = hexToRgb('#'+color);
+    const backgroundColor: string = 'rgb(' + colorObj.r.toString() + ',' + colorObj.g.toString() + ',' + colorObj.b.toString() + ')';
     //**background: linear-gradient(90deg, rgba(238,174,202,1) 0%, rgba(148,187,233,1) 100%);
     const gradient: any = {
-        r: color.r-100,
-        g: color.g-100,
-        b: color.b-100,
+        r: colorObj.r-100,
+        g: colorObj.g-100,
+        b: colorObj.b-100,
     };
-    const gradientString: string = 'linear-gradient(90deg, rgba(' +color.r.toString() + ',' + color.g.toString() + ',' + color.b.toString() + ', 1) 0%, rgba(' + gradient.r.toString() + ',' + gradient.g.toString() + ',' + gradient.b.toString() + ', 1) 100%)';
+    const gradientString: string = 'linear-gradient(90deg, rgba(' +colorObj.r.toString() + ',' + colorObj.g.toString() + ',' + colorObj.b.toString() + ', 1) 0%, rgba(' + gradient.r.toString() + ',' + gradient.g.toString() + ',' + gradient.b.toString() + ', 1) 100%)';
     const classes = useStyle({backgroundColor, gradientString});
     return (
         <div className={classes.root}>
